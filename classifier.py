bf2940fee256415856cf354ec56ef6bcee8042d4
unknown <98kwcho@gmail.com>
2019-08-27 13:01:04 +0900

    2번째 연습

---
+++ classifier.py
@@ -0,0 +1,46 @@
+from konlpy.tag import Kkma
+from nltk.tag import pos_tag
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
+#from konlpy.utils import pprint 프린트 출력 관련
+from googletrans import Translator
+
+def Kor_classifier(text):
+    kkma = Kkma()
+    result = (kkma.pos(text))
+    return result
+
+def Eng_classifier(text):
+    word = word_tokenize(text)
+    tag = nltk.pos_tag(word)
+    return tag
+
+def Encryption(text):
+    translator = Translator()
+    result = translator.translate(text, src='ko', dest = 'en')
+    return result.text
+
+def Decryption(text):
+    translator = Translator()
+    result = translator.translate(text, src='en', dest = 'ko')
+    return result.text
+
+string = input("문장 입력")
+
+kor = Kor_classifier(string)
+enc = Encryption(string)
+eng = Eng_classifier(enc)
+dec = Decryption(string)
+
+print(kor)
+
+temp = [ ]
+for key, value in kor:
+        temp.append(key)
+
+print(temp)
+print(enc)
+print(eng)
+print(dec)
+
